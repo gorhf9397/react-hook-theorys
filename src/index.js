50af21f9b1fcd71b6c3bb1814b092d7a9a9bda62
seokgeun <trr456@naver.com>
2022-11-10 21:24:28 +0900

    useInput 추가작성

--- src/index.js
+++ src/index.js
@@ -4,16 +4,27 @@ import ReactDOM from "react-dom";
 import "./style.css";
 
 // hook 을 사용하면 function 안에서 event 를 처리할수있음
-const useInput = (initialValue) => {
+const useInput = (initialValue, validator /*유효성을 확인하는 명령어 */) => {
   const [value, setValue] = useState(initialValue);
-  const onChange = e => {
-    console.log(e.target);
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      // validator 과 function 이 같으면 willUpdate 에 validator결과를 업로드
+      willUpdate = validator(value); //value에 대한 유효성 검사를 하는 if문
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
   };
   return { value, onChange }; // 어떤 변화를 주기전에 기본값(Mr.kim)을 value와 함께 return
 };
 
 const App = () => {
-  const name = useInput("Mr.kim");
+  const maxLen = (value) => value.length <= 10; // value의 길이를 체크
+  const name = useInput("Mr.kim", maxLen);
   return (
     <div className="App">
       <h1>Hello</h1>
