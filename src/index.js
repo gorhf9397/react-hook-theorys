3fd09bfbcffc0aa70e4820f6fcc72f8b89f2a256
seokgeun <trr456@naver.com>
2022-11-13 19:45:58 +0900

    onConfirm window.confirm명령어추가

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ import ReactDOM from "react-dom";
 import "./style.css";
 
 export const useConfirm = (message = "", onConfirm, onCancel) => {
-  if(!onConfirm || typeof onConfirm !== "function") {
-     return;
+  if (!onConfirm || typeof onConfirm !== "function") {
+    return;
   }
-  if(onCancel && typeof onCancel !== "function") {
-    return; 
+  if (onCancel && typeof onCancel !== "function") {
+    return;
   }
   const confirmAction = () => {
-    if(confirm(message)) {
-      onConfirm();  
+    if (window.confirm(message)) {
+      onConfirm();
     } else {
       onCancel();
     }
-  }
+  };
   return confirmAction;
-}
+};
 
 const App = () => {
   const deleteWorld = () => console.log("Deleteing World");
@@ -26,7 +26,7 @@ const App = () => {
   const confirmDelete = useConfirm("Are you sure", deleteWorld, abort);
   return (
     <div className="App">
-      <button>Delete the world</button>
+      <button onClick={confirmDelete}>Delete the world</button>
     </div>
   );
 };
